6998bbd114477ecf91ae6cb03da59a7f94ef8b2e
deepakpk7 <deepakpk7034@gmail.com>
2024-08-12 13:34:58 +0530

    new file

---
+++ sample.py
@@ -0,0 +1,16 @@
+print("Hello world")
+a=10
+print(10)
+b=True
+c=False
+d=["list"]
+e={"set"}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+f={'name':'Deepak','age':20}
+print(f)
+

5d5d4e76fbfad76fd1c671489567d99046c14d9d
deepakpk7 <deepakpk7034@gmail.com>
2024-08-14 13:28:22 +0530

    String

--- operator.py
+++ operator.py
@@ -1,49 +1,89 @@
 # ARITHEMATIC OPERATOR
 
-print(5+5)
-print(10-5)
-print(10/5)
-print(50%5)
-print(5*5)
-print('Deepak '*2)
-print(5//5)
-print(5**5)
-print()
+# print(5+5)
+# print(10-5)
+# print(10/5)
+# print(50%5)
+# print(5*5)
+# print('Deepak '*2)
+# print(5//5)
+# print(5**5)
+# print()
 
 # Assignment Operator
-print("Assignment ")
-print("-------------------")
+# print("Assignment ")
+# print("-------------------")
 
-a=10
-a+=5
-print(a)
+# a=10
+# a+=5
+# print(a)
 
-b=15
-b-=5
-print(b)
+# b=15
+# b-=5
+# print(b)
 
-c=5
-c*=5
-print(c)
+# c=5
+# c*=5
+# print(c)
 
-d=5
-d**=5
-print(d)
+# d=5
+# d**=5
+# print(d)
 
-e=20
-e/=4
-print(e)
+# e=20
+# e/=4
+# print(e)
 
-f=20
-f//=3
-print(f)
+# f=20
+# f//=3
+# print(f)
 
 # COMPARISON OPERATOR
-print("Comparison Operator")
-print("-------------------")
-print(15==15)
-print(15!=4)
-print(15<4)
-print(15<=15)
-print(15>14)
+# print("Comparison Operator")
+# print("-------------------")
+# print(15==15)
+# print(15!=4)
+# print(15<4)
+# print(15<=15)
+# print(15>14)
+
+# LOGICAL OPERATOR
+# print("LOGICAL OPERATOR")
+# print("-------------------")
+# print(10==10 and 10<11)
+# print(10==10 and 15<11)
+# print(10==90 and 10<11)
+# print()
+# print(10==9 or 10==9)
+# print(10==10 or 10==10)
+# print()
+# print(not(10==9))
+
+# MEMBERSHIP OPERATOR
+# print("-------------------")
+# a="deepak"
+# b='d'
+# print(b in a)
+
+# print(b not in a)
+# c=[28,825,56,904,765]
+# print(20 in c)
+
+# print("Identity Operator")
+# print("-------------------")
+# a=10
+# b=12
+# print(id(a))
+# print(id(b))
+# print(a is b)
+# print(a is not b)
+
+# print(id(20))
+
+a="deepak "
+b="python "
+print(a==b)
+print(b in a)
+print('d' in a and 'a' not in b)
+print(b+a)
 
---
+++ string.py
@@ -0,0 +1,9 @@
+# STRING
+
+a="deepak"
+print(a[1])
+print(a[-1])
+print("Length=",len(a))
+print("Python ","\nFullstack")
+b="""python
+fullstack"""

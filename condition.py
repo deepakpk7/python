4ae0d1e48604abf56b255ff2a796cdf1b498f3f5
deepakpk7 <deepakpk7034@gmail.com>
2024-08-17 13:27:19 +0530

    python question

--- condition.py
+++ condition.py
@@ -4,7 +4,7 @@
 #     print("Equal")
 # else:
 #     print("not equal")
-
+# print("-------------------")
 # Largest 
 
 # a=int(input("Enter a number "))
@@ -14,12 +14,14 @@
 # else :
 #     print("Largest is ",b)
 
-
+# print("-------------------")
 # age=int(input("enter your age :"))
 # if age>=18:
 #     print("eligible ")
 # else:
 #     print("Not Eligible")
+
+# print("-------------------")
 
 # Odd or Even
 # n=int(input("Enter a number :"))
@@ -27,6 +29,7 @@
 #     print("Even")
 # else:
 #     print("Odd")
+# print("-------------------")
 
 
 # s=input("enter a string: ")
@@ -36,6 +39,8 @@
 # else :
 #     print("Not visible")
 
+# print("-------------------")
+
 # salary=int(input("enter salary :"))
 # y=int(input("year of service :"))
 # if y>=5 :
@@ -44,9 +49,98 @@
 # else :
 #     print("not eligible")
 
-n=int(input("enter a number"))
-l=n%10
-if l%3==0:
-    print("possible")
-else :
-    print("not possible")
+# print("-------------------")
+
+# n=int(input("enter a number"))
+# l=n%10
+# if l%3==0:
+#     print("possible")
+# else :
+#     print("not possible")
+
+# print("-------------------")
+
+# a=int(input("Enter a 1st no :"))
+# b=int(input("Enter a 2st no :"))
+
+# if a==b:
+#     print("equal")
+# elif a>b :
+#     print(a)
+# else:
+#     print(b)
+
+# print("-------------------")
+
+# num=int(input("enter a no :"))
+# if num==1:
+#     print("Sunday")
+# elif num==2:
+#     print("Monday")
+# elif num==3:
+#     print("Tuesday")
+# elif num==4:
+#     print("Wendsday")
+# elif num==5:
+#     print("Thuresday")
+# elif num==6:
+#     print("Friday")
+# elif num==7:
+#     print("Saturday")
+# else:
+#     print("Not Valid")
+
+# print("-------------------")
+
+# city=input("enter a city :")
+# if city=='delhi' :
+#     print("Mounment red fort")
+# elif city=='agra':
+#     print("Mounment taj mahal")
+# elif city=='Mounment jaipur':
+#     print("Mounment jal mahal")
+# else:
+#     print("Invalid")
+
+# print("-------------------")
+
+# price=int(input("enter bike price : "))
+# if price>=100000:
+#     t=price*0.15
+#     print("Tax is 15% =",t)
+# elif price>=50000 and price<=100000 :
+#     t=price*0.10
+#     print("Tax is 10% =",t)
+# else :
+#     price<=50000
+#     t=price*0.05
+#     print("Tax is 5% =",t)
+
+# print("-------------------")
+
+# unit=int(input("Enter Units : "))
+# if unit<=100:
+#     print("No charge")
+# elif unit<=200:
+#     b=(unit-100)*5
+#     print(b)
+# else :
+#     b=(unit-200)*10+500
+#     print(b)
+
+# print("-------------------")
+
+a=int(input("Enter 1st : "))
+b=int(input("Enter 2st : "))
+c=int(input("Enter 3rd : "))
+if a>b:
+    if a>c:
+        print(a)
+    else :
+        print(c)
+else:
+    if b>c:
+        print(b)
+    else :
+        print(c)
+

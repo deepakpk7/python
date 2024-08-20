7f707a798a5ab34b5decb4e852406f219ec05406
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-08-20 13:33:04 +0530

    String reverse using while loop

--- loop.py
+++ loop.py
@@ -40,20 +40,68 @@
 #     i+=1
 
 
-i=int(input("Enter a first no : "))
-e=int(input("Enter a last number : "))
-sum=0
-sum1=0
-sum2=0
-while i<=e:
-    sum=sum+i
-    if i%2==0:     
-        sum1=sum1+i
-    else :
-        sum2=sum2+i
+# i=int(input("Enter a first no : "))
+# e=int(input("Enter a last number : "))
+# sum=0
+# sum1=0
+# sum2=0
+# while i<=e:
+#     sum=sum+i
+#     if i%2==0:     
+#         sum1=sum1+i
+#     else :
+#         sum2=sum2+i
+#     i+=1
+# print(sum)
+# print(sum1)
+# print(sum2)
+
+# FACTORIAL OF A NUMBER
+
+# a=int(input("Enter a number : "))
+# i=1
+# f=1
+# while i<=a:
+#     f*=i
+#     i+=1
+# print(f)
+
+# REVERSE A NUMBER
+
+# n=int(input("Enter a no :: "))
+# r=0
+# while n>0:
+#     d=n%10
+#     r=r*10+d
+#     n//=10
+# print("Reverse = ",r)
+
+# SUM OF A DIGIT
+
+# n=int(input("Enter a no :: "))
+# s=0
+# while n>0:
+#     d=n%10
+#     s=s+d
+#     n//=10
+# print("Sum of digit =",s)
+
+# STRING
+
+# s=input("Enter a string : ")
+# l=len(s)
+# i=0
+# while i<l:
+#     print(s[i])
+#     i+=1
+
+# STRING REVERSE
+
+s=input("Enter a string : ")
+l=len(s)
+i=0
+r=''
+while i<l:
+    r=s[i]+r 
     i+=1
-print(sum)
-print(sum1)
-print(sum2)
-
-
+print(r)

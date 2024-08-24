bff133b4d54784b316b2485a7c015faf694851bc
deepakpk7 <deepakpk7034@gmail.com>
2024-08-24 13:28:52 +0530

    Nested for loop

--- for.py
+++ for.py
@@ -94,8 +94,73 @@
 
 # STRING REVERSE
 
-s=input("Enter a string : ")
-r=''
-for i in s:
-    r=i+r 
-print("Reverse a String :",r)
+# s=input("Enter a string : ")
+# r=''
+# for i in s:
+#     r=i+r 
+# print("Reverse a String :",r)
+
+# for i in range(4):
+#     print("i=",i)
+#     for j in range(3):
+#         print("\tj=",j)
+
+# for i in range(5):
+#     for j in range(5):
+#         print(j,end="\t")
+#     print()
+
+# for i in range(5):
+#     a=1
+#     for j in range(23):
+#         print(a,end="\t")
+#         a+=1
+#     print("*")
+    
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="\t")
+#     print()
+
+""" 0 0 0
+    1 1 1
+    2 2 2"""
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end="\t")
+#         i+=1
+#     print()
+
+"""
+    0 1 2
+    1 2 3
+    2 3 4
+"""
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="\t")
+#         a+=1
+#     print()
+    
+
+"""
+    0 1 2
+    3 4 5
+    6 7 8 """
+
+
+for i in range(4):
+    if i%2==0:
+        for j in range(3):
+            print(j,end="\t")
+    else:
+        for j in range(3):
+            print(2-j,end="\t")
+    print()
+"""
+    0   1   2
+    2   1   0
+    0   1   2
+    2   1   0"""

51edede4f584e535a9bee2b62ecc3edd9980a461
deepakpk7 <deepakpk7034@gmail.com>
2024-08-28 13:23:38 +0530

    updates

--- list.py
+++ list.py
@@ -2,12 +2,12 @@
 # for i in l:
 #     print(i)
 
-"""
-10
-11
-anu
-5.0
-"""
+# """
+# 10
+# 11
+# anu
+# 5.0
+# """
 
 
 # l=[10,11,'anu',5.0]
@@ -16,31 +16,71 @@ anu
 # else:
 #     print("NO")
 
-"""
-YES
-"""
+# """
+# YES
+# """
 
 # l=[10,11,'anu',5.0]
 # print(l[2])
 # l[2]="Deepak"
 # print(l)
 
-"""
-anu
-[10, 11, 'Deepak', 5.0]
-"""
+# """
+# anu
+# [10, 11, 'Deepak', 5.0]
+# """
 
-l=[10,11,'anu',5.0]
-l.append('append')
-print(l)
-l.extend(['a','b','c'])
-print(l)
-l.insert(0,"insert")
-print(l)
+# ADD
+# l=[10,11,'anu',5.0]
+# l.append('append')
+# print(l)
+# l.extend(['a','b','c'])
+# print(l)
+# l.insert(0,"insert")
+# print(l)
 
-"""
-[10, 11, 'anu', 5.0, 'add']
-[10, 11, 'anu', 5.0, 'add', 'a', 'b', 'c']
-['insert', 10, 11, 'anu', 5.0, 'add', 'a', 'b', 'c']
-"""
+# """
+# [10, 11, 'anu', 5.0, 'add']
+# [10, 11, 'anu', 5.0, 'add', 'a', 'b', 'c']
+# ['insert', 10, 11, 'anu', 5.0, 'add', 'a', 'b', 'c']
+# """
+
+# l=[10,11,'anu',5.0]
+# l.pop()
+# print(l)
+# """
+# [10, 11, 'anu']
+# """
+# l.pop(2)
+# print(l)
+# """
+# [10, 11]
+# """
+# l.remove(10)
+# print(l)
+# """
+# [11]
+# """
+# l.clear()
+# print(l)
+
+# l=[4,2,9,8,3,11,32]
+# l.sort()
+# print(l)
+# l.reverse()
+# print(l)
+# """
+# [2, 3, 4, 8, 9, 11, 32]
+# [32, 11, 9, 8, 4, 3, 2]
+# """
+# l1=l.copy()
+# l1.pop(4)
+# print(l)
+# print(l1)
+
+# print(l.index(2))
+# """
+# 1
+# """
+
 

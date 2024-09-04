351526bd7d1afd3dabe3e4bb0f0429374055c75c
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-09-04 13:00:33 +0530

    Update class_task.py

--- Task/class_task.py
+++ Task/class_task.py
@@ -10,43 +10,57 @@ while True:
     6. Exit
     """)
     choice = int(input("Enter Your Choice: "))
+    
     if choice == 1:
         name = input("Enter a Name: ")
         bar_code = int(input("Enter bar_code: "))
         price = int(input("Enter price: "))
-        products.append({'bar_code':bar_code,'price':price})
+        products.append({'name': name, 'bar_code': bar_code, 'price': price})
         print(products)
+    
     elif choice == 2:
         print('_' * 30)
-        print('{:<10}{:<5}{:<5}'.format('Name', 'bar_code', 'price'))
+        print('{:<10}{:<10}{:<10}'.format('Name', 'bar_code', 'price'))
         print('*' * 30)
         for i in products:
-            print('{:<10}{:<5}{:<5}'.format(name, i['bar_code'], i['price']))
+            print('{:<10}{:<10}{:<10}'.format(i['name'], i['bar_code'], i['price']))
+    
     elif choice == 3:
-        name = input("Enter a Name: ")
-        if name in products:
-            bar_code = int(input("Enter a new bar_code: "))
-            price = int(input("Enter a new price: "))
-            products[name] = {'bar_code': bar_code, 'price': price}
+        name = input("Enter the Name of the product to update: ")
+        for i in products:
+            if i['name'] == name:
+                bar_code = int(input("Enter a new bar_code: "))
+                price = int(input("Enter a new price: "))
+                i['bar_code'] = bar_code
+                i['price'] = price
+                print("Product updated successfully.")
+                break
         else:
-            print("Name Not in the Database")
+            print("Name not in the Database")
+    
     elif choice == 4:
         name = input("Enter the product name to Delete: ")
-        f=0
-        if name in products:
-            products.pop(name)
-            f=1
-        if f==0:
-            print("product Not in the Database")
-    elif choice == 5:
-        name = input("Enter a Name: ")
-        if name in products:
-            print('{:<10}{:10}{:<10}'.format('Name','bar_code','price'))
-            print('{:<10}{:10}{:<10}'.format(name, products[name]['bar_code'], products[name]['price']))
+        for i in products:
+            if i['name'] == name:
+                products.remove(i)
+                print("Product removed successfully.")
+                break
         else:
-            print("***product not found***")
+            print("Product not in the Database")
+    
+    elif choice == 5:
+        name = input("Enter the product name to Search: ")
+        for i in products:
+            if i['name'] == name:
+                print('{:<10}{:<10}{:<10}'.format('Name', 'bar_code', 'price'))
+                print('{:<10}{:<10}{:<10}'.format(i['name'], i['bar_code'], i['price']))
+                break
+        else:
+            print("***Product not found***")
     
     elif choice == 6:
         break
+    
     else:
         print("Invalid Choice")
+

2f469f4b0c9af312243681bfa3ae617c574136d1
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-09-06 13:34:28 +0530

    list and tuple

---
+++ set.py
@@ -0,0 +1,30 @@
+# s=set()
+# s1={1,2,3,4,'abc',1}
+# print(type(s1))
+
+# l=[1,2,3,1,2,3,4]
+# print(l)
+# s=set(l)
+# print(s)
+# l=list(s)
+# print(l)
+
+# s={1,2,3}
+# s.add(0)
+# print(s)
+# s1={0,10,1,2,3}
+# print(s.difference(s1))
+# s1.clear()
+# print(s1)
+# s.discard(3)
+# print(s)
+# s.remove(1)
+# print(s)
+# s.pop()
+# print(s)
+# s.update({3,4,5,6})
+# print(s)
+
+s={1,2,3,4,6}
+s1={1,2,3,6}
+print(s.difference(s1))
---
+++ tuple.py
@@ -0,0 +1,30 @@
+# t=(1,2,3,'ab',5.8)
+# for i in t:
+#     print(i)
+# if 2 in t:
+#     print("true")
+# else:
+#     print("False")
+# print(t[3])
+
+# t=1,2,3,4
+# print(t)
+# t=(10,)
+# print(type(t))
+# t=(1,2,[10,11])
+# print(t[2])
+# t[2].append(12)
+# print(t)
+# print(t.count(2))
+# print(t.index())
+
+# t=(1,2,3,4)
+# l=list(t)
+# l.pop()
+# print(l)
+# t=tuple(l)
+# print(t)
+
+d={'name':'abc','age':21}
+l=list(d)
+print(type(l),l)

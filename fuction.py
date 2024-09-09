9a7f58ab2eebd096d388483e6a89c75923ac5630
deepakpk7 <deepakpk7034@gmail.com>
2024-09-09 12:30:05 +0530

    function

---
+++ fuction.py
@@ -0,0 +1,22 @@
+# def fun():
+#     print("python")
+#     print("HTML")
+#     print("Fullstack")
+    
+# fun()
+# print("*"*10)
+# fun()
+# print("*"*10)
+# fun()
+
+
+def fun1():
+    a=10 #local variable
+    print(a)
+    print(b)
+a=5
+b=20 #global variable
+print(b)
+print(a)
+
+fun1()

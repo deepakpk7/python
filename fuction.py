46cf798c2439906177e9df77323d8ca2994c4392
deepakpk7 <deepakpk7034@gmail.com>
2024-09-09 13:27:31 +0530

    function

--- fuction.py
+++ fuction.py
@@ -10,13 +10,39 @@
 # fun()
 
 
-def fun1():
-    a=10 #local variable
-    print(a)
-    print(b)
-a=5
-b=20 #global variable
-print(b)
-print(a)
+# def fun1():
+#     a=10 #local variable
+#     print(a)
+#     print(b)
+# a=5
+# b=20 #global variable
+# print(b)
+# print(a)
 
-fun1()
+# fun1()
+
+# def fun2():
+#     global a
+#     a=10
+#     print(a)
+# print("Local into Global",a)
+# fun2()
+
+
+# def fun3():
+#     return 'welcome',10
+
+# a,b=fun3()
+# print(a)
+# print(b)
+
+
+def fun4():
+    a=1
+    b=2
+    c=3
+    return a,b,c
+a1,b2,c3=fun4()
+print(a1)
+print(b2)
+print(c3)

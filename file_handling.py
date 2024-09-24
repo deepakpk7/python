8a093bc00b3437261f0dfb881a0c08fc0a720c4d
jerikejoseph <jerikejoseph144@gmail.com>
2024-09-24 12:54:43 +0530

    file

--- file_handling.py
+++ file_handling.py
@@ -1,4 +1,48 @@
 # f=open('python/dpk.txt','x')
-f=open('python/dpk.txt','r')
+# f=open('python/dpk.txt','r')
 # f.write('Welcome to VS Code')
-print(f.read())
+# print(f.read())
+
+# a=f.read(5)
+
+# f.seek(3)
+# b=f.read()
+# print(f.tell())
+# print(a)
+# print("_"*20)
+# print(b)
+
+# f=open('python/dpk.txt','r')
+# a=f.readline(5)
+# print(a)
+# b=f.readline()
+# print(b)
+# f.seek(0)
+# c=f.readlines()
+# print(c)
+
+# f=open('python/dpk.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     print(a)
+    
+# f=open('python/dpk.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     # print(a[::-1])
+#     r=''
+#     for i in a:
+#         r=i+r
+#     print(r)
+
+f=open('python/dpk2.txt','a')
+f.write('New file using append')
+
+    
+    
+
+

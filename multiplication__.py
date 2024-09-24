4c3f23e44c9d8c32765b37501d61db318e4b9d4c
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-09-25 00:48:30 +0530

    update

--- multiplication__.py
+++ multiplication__.py
@@ -1,11 +1,19 @@
 f=open('python/dpk2.txt','a')
 
 m=int(input("Enter a multiplication :"))
-i=1
-for i in range(i,10+1):
+# i=1
+# for i in range(i,10+1):
     # b=i,"*",m,"=",i*m
     # b=str(b)
     # f.write(b)
-    f.write(str(i)+'*'+str(m)+'='+str(i*m)+'\n')
+    # f.write(str(i)+'*'+str(m)+'='+str(i*m)+'\n')
+
+
+for i in range(1,m+1):
+    for j in range(1,11):
+        f.write(str(j)+ '*'+str(i)+' = '+str(i*j) + '\n')
+    f.write('\n')
+
+
     
 

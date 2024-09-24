54921a27e19f7496ad94a92fbf24a938475e2ced
deepakpk7 <deepakpk7034@gmail.com>
2024-09-24 13:31:26 +0530

    file handling

---
+++ multiplication__.py
@@ -0,0 +1,11 @@
+f=open('python/dpk2.txt','a')
+
+m=int(input("Enter a multiplication :"))
+i=1
+for i in range(i,10+1):
+    # b=i,"*",m,"=",i*m
+    # b=str(b)
+    # f.write(b)
+    f.write(str(i)+'*'+str(m)+'='+str(i*m)+'\n')
+    
+

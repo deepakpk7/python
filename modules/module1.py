3569caa5f5d07203e6bdfe266c304ae4ced56cd4
deepakpk7 <deepakpk7034@gmail.com>
2024-09-26 18:07:56 +0530

    Modules

---
+++ modules/module1.py
@@ -0,0 +1,8 @@
+def fun1():
+    print("Hey i am here...")
+
+def fun2():
+    print("welcome")
+    
+a='Good Morning'
+l=''
---
+++ modules/modulr2.py
@@ -0,0 +1,22 @@
+# FIRST METHOD
+# import module1
+# module1.fun1() 
+# module1.fun2()
+# print(module1.a)
+
+# SECOND METHOD 
+# from module1 import fun1,fun2,a
+# fun1()
+# fun2()
+# print(a)
+
+# THIRD METHOD
+# from module1 import *
+# print(a)
+# fun1()
+# fun2()
+
+import module1 as m
+m.fun1()
+m.fun2()
+m.a

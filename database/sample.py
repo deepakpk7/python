731ddf58b4e0e8326a0a4cdc798a8ccecc96c1cc
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-10-08 11:35:23 +0530

    add

--- database/sample.py
+++ database/sample.py
@@ -7,50 +7,83 @@ try:
 except:
     print("Table exist")
     
-con.execute("insert into student(roll_no,name,age,mark)values(1,'Deepak',20,80),(2,'ibin',21,68),(3,'alen',21,78),(4,'sreehari',21,87),(5,'alma',21,98)")
-con.commit()
+# con.execute("insert into student(roll_no,name,age,mark)values(1,'Deepak',20,80),(2,'ibin',21,68),(3,'alen',21,78),(4,'sreehari',21,87),(5,'alma',21,98)")
+# con.commit()
 
-# n=int(input("enter the limit"))
+# roll=int(input("Enter roll no :"))
+# name=input("Enter Name :")
+# age=int(input("Enter the age :"))
+# mark=float(input("Enter mark :"))
+
+# con.execute('insert into student(roll_no,name,age,mark)values(?,?,?,?)',(roll,name,age,mark))
+# con.commit()
+
+
+# n=int(input("enter the limie :"))
 # print("*"*20)
 # for i in range(n):
-#     roll=int(input("enter roll no :"))
-#     name=input("enter name :")
-#     age=int(input("enter thr age :"))
-#     mark=float(input("enter the mark :"))
-
+#     roll=int(input("Enter roll no :"))
+#     name=input("Enter Name :")
+#     age=int(input("Enter the age :"))
+#     mark=float(input("Enter mark :"))
 #     con.execute('insert into student(roll_no,name,age,mark)values(?,?,?,?)',(roll,name,age,mark))
 #     con.commit()
 
+# SELECT * FROM
+
 # data=con.execute("select * from student")
+# # print(data)
 # print('_' * 60)
-# print('{:<10}{:<10}{:<10}{:<10}'.format('ROLL NO','NAME','AGE','MAEK'))
+# print('{:<10}{:<10}{:<10}{:<10}'.format('ROLL NO','NAME','AGE','MARK'))
 # print('*' * 60)
 # for i in data:
 #     # print(i[0])
 #     print('{:<10}{:<10}{:<10}{:<10}'.format(i[0],i[1],i[2],i[3]))
     
-
+    
 # USING CONDITION
-# n=int(input("enter the  value"))
 
-# data=con.execute("select * from student where roll_no=?",(n,))
+# data=con.execute("select * from student where roll_no=2")
+# # print(data)
 # print('_' * 60)
-# print('{:<10}{:<10}{:<10}{:<10}'.format('ROLL NO','NAME','AGE','MAEK'))
+# print('{:<10}{:<10}{:<10}{:<10}'.format('ROLL NO','NAME','AGE','MARK'))
 # print('*' * 60)
 # for i in data:
-#     # print(i[0]) 
-#       print('{:<10}{:<10}{:<10}{:<10}'.format(i[0],i[1],i[2],i[3]))
+#     print('{:<10}{:<10}{:<10}{:<10}'.format(i[0],i[1],i[2],i[3]))
+    
     
 
- 
-# updating
+
+# print(data)
+# n=int(input("Enter the value :"))
+# data=con.execute("select * from student where roll_no=?",(n,))
+
+# print('_' * 60)
+# print('{:<10}{:<10}{:<10}{:<10}'.format('ROLL NO','NAME','AGE','MARK'))
+# print('*' * 60)
+# for i in data:
+#     print('{:<10}{:<10}{:<10}{:<10}'.format(i[0],i[1],i[2],i[3]))
+
+# USING IF 
+# for i in data:
+#     if n==i[0]:
+#         print('{:<10}{:<10}{:<10}{:<10}'.format(i[0],i[1],i[2],i[3]))
+
+# UPDATING 
 
 
-# name=input("enter the name :" )
-# n_name=input("enter the second name : ")
-# con.execute("update student set name=? where name=?",(n_name,name))
+# name=input("Target name :")
+# nname=input("Enter new name: ")
+# con.execute("update student set name=? where name=? ",(nname,name))
 # con.commit()
 
-r=input("enter the deletd student roll no :")
-con.execute("delet from student where roll_no=?",(r,))
+r=input("Enter the deiete student Roll no :")
+con.execute("delete from student where roll_no=? ",(r,))
 con.commit()
+
+    
+    
+
+
+
+    

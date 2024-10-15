97dc3ae00487f8302331ff791dfed10eceb49a69
Deepak P K <137987354+deepakpk7@users.noreply.github.com>
2024-10-15 13:17:49 +0530

    update

--- Tkinter/Login.py
+++ Tkinter/Login.py
@@ -38,12 +38,34 @@ def register():
     b1.place(x=150,y=150)
     win1.mainloop()
 
+# print username and password   
+# def login():
+#     print('username: ',e1.get())
+#     l2. config(text=e1.get())
+#     print('Password :',e2.get())
+#     l3.config(text=e2.get())
+
+def Home():
+    win2=tkinter.Tk()
+    l1=tkinter.Label(win2,text="Home Page")
+    l1.pack()
+    win2.minsize(500,500)
+    win2.maxsize(800,800)
+    win2.configure(bg="black")
+    b1=tkinter.Button(win2,text="Logout",command=win2.quit)
+    b1.pack()
+    win2.mainloop()
+
+def login():
+    con=sqlite3.connect("python/Tkinter/user.db")
+    data=con.execute("select * from user where uname=? and password=?",(e1.get(),e2.get()))
+    f=0
+    for i in data:
+        f=1
+        Home()
+    if f==0:
+        l4.config(text="INVALID USERNAME OR PASSWORD")
     
-def save():
-    print('username: ',e1.get())
-    l2. config(text=e1.get())
-    print('Password :',e2.get())
-    l3.config(text=e2.get())
 
 l1=tkinter.Label(win,text="Login Page",bg="green",fg="black")
 l1.pack()
@@ -58,6 +80,9 @@ e2.place(x=200,y=70)
 
 b1=tkinter.Button(win,text="Register",bg="white",activebackground="white",fg="black",activeforeground="red",command=register)
 b1.place(x=150,y=100)
-b2=tkinter.Button(win,text="Login",bg="white",activebackground="white",fg="black",activeforeground="red",command=save)
+b2=tkinter.Button(win,text="Login",bg="white",activebackground="white",fg="black",activeforeground="red",command=login)
 b2.place(x=250,y=100)
+
+l4=tkinter.Label(win)
+l4.place(x=200,y=200)
 win.mainloop()
